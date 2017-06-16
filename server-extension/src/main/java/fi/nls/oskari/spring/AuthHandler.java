456231c8632cf62cf4de9228324fc3022cd36d19
Sami Mäkinen <sami.makinen@nls.fi>
2017-06-16 13:58:12 +0300

    Hex to readable string testing

--- server-extension/src/main/java/fi/nls/oskari/spring/AuthHandler.java
+++ server-extension/src/main/java/fi/nls/oskari/spring/AuthHandler.java
@@ -8,30 +8,55 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.xml.bind.DatatypeConverter;
+import java.nio.charset.Charset;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by SMAKINEN on 2.6.2017.
- */
 @Controller
 @RequestMapping("/auth")
 public class AuthHandler {
 
     private final static Logger LOG = LogFactory.getLogger(AuthHandler.class);
 
+    /*
+        Headers:
+        "auth-email":"asdf@asdf.fi",
+        "auth-firstname":"asdf",
+        "auth-lastname":"0xNNNNNNNN",
+        "auth-screenname":"asdf",
+        "auth-nlsadvertisement":""
+    */
     @RequestMapping
     public @ResponseBody
     Map<String,String> index(@OskariParam ActionParameters params) throws Exception {
+        LOG.info("User logged in:", params.getRequest().getHeader("auth-email"));
         Enumeration<String> names = params.getRequest().getHeaderNames();
         Map<String, String> headers = new HashMap<>();
         while(names.hasMoreElements()) {
             final String key = names.nextElement().toLowerCase();
             if(key.startsWith("auth") || "true".equalsIgnoreCase(params.getHttpParam("all"))) {
-                headers.put(key, params.getHttpHeader(key));
+                headers.put(key, getValue(params.getHttpHeader(key)));
             }
         }
         return headers;
+    }
+
+    /**
+     * Some values can be encoded
+     * @param input
+     * @return
+     */
+    public String getValue(String input) {
+        if(input == null) {
+            return null;
+        }
+        if(!input.startsWith("0x")) {
+            return input;
+        }
+        input = input.substring(2);
+        byte[] bytes = DatatypeConverter.parseHexBinary(input);
+        return new String(bytes, Charset.forName("UTF-8"));
     }
 }
