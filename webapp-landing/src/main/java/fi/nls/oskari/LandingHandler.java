64b74a5e1f94d0e8f8b62ea156faf1f820b672b4
Sami Mäkinen <sami.makinen@nls.fi>
2017-06-26 10:41:02 +0300

    Logging for broken urls

--- webapp-landing/src/main/java/fi/nls/oskari/LandingHandler.java
+++ webapp-landing/src/main/java/fi/nls/oskari/LandingHandler.java
@@ -1,7 +1,11 @@
 package fi.nls.oskari;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by SMAKINEN on 2.6.2017.
@@ -9,9 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class LandingHandler {
+    private static final Logger logger404 = Logger.getLogger("BrokenURL");
 
     @RequestMapping
     public String index() throws Exception {
         return "landingpage";
+    }
+
+    @RequestMapping("**")
+    public ModelAndView handleAnyRequest(HttpServletRequest request) {
+        StringBuilder b = new StringBuilder(request.getRequestURI());
+        final String query = request.getQueryString();
+        if(query != null) {
+            b.append("?");
+            b.append(query);
+        }
+        logger404.info(b.toString() + " referer: " + request.getHeader("referer"));
+        return new ModelAndView("redirect:/");
     }
 }
