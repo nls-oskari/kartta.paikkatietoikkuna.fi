75af3c765727380bd4cbe825f0e564fa9e947ff7
Sami Mäkinen <sami.makinen@nls.fi>
2017-06-29 15:15:37 +0300

    Update Turku opaskartta to https

---
+++ app-resources/src/main/resources/flyway/ptimigration/V1_10__update_layer_urls_from_http_to_https.sql
@@ -0,0 +1,10 @@
+-- Update opaskartta.turku.fi to https so they are not proxied
+UPDATE oskari_maplayer
+SET url = 'https' || substring(url from 5)
+WHERE url LIKE 'http://opaskartta.turku.fi/%' AND type='wmslayer';
+
+UPDATE oskari_resource
+SET resource_mapping = 'wmslayer+https' || substring(resource_mapping from 16)
+  WHERE resource_mapping LIKE 'wmslayer+http://opaskartta.turku.fi%';
+
+DELETE FROM oskari_capabilities_cache WHERE url LIKE 'http://opaskartta.turku.fi%' AND layertype='wmslayer';
