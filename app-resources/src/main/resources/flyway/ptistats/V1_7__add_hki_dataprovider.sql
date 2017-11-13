12338efcaf8352020c4fa4352cf1fea45a3641d2
Sami Mäkinen <sami.makinen@nls.fi>
2017-11-13 13:53:51 +0200

    HKI stats datasources

---
+++ app-resources/src/main/resources/flyway/ptistats/V1_7__add_hki_dataprovider.sql
@@ -0,0 +1,50 @@
+
+-- datasource for "PxWEB on City of Helsinki"
+INSERT INTO oskari_statistical_datasource (locale, config, plugin)
+VALUES ('{"fi":{"name":"Helsingin kaupunki"},"en":{"name":"City of Helsinki"}}"',
+'{"url":"http://api.aluesarjat.fi/PXWeb/api/v1/fi/Helsingin%20seudun%20tilastot/P%C3%A4%C3%A4kaupunkiseutu%20alueittain","info":{"url":"http://api.aluesarjat.fi"},"regionKey":"Alue","ignoredVariables":["Alue"]}', 'PxWEB');
+
+-- Add layers
+INSERT INTO oskari_maplayer(type, url,
+                    name, groupId,
+                    locale,
+                    attributes, opacity, srs_name, version)
+VALUES(
+    'statslayer', 'http://geoserver.hel.fi/geoserver/wms',
+    'seutukartta:Seutu_suuralueet', (select id from oskari_layergroup where locale LIKE '%Yhteistyöaineistot%'),
+    '{ "en" : {
+         "name":"Pääkaupunkiseudun suuraluejako"
+       },"fi" : {
+         "name":"Pääkaupunkiseudun suuraluejako"
+    }}',
+    '{"statistics":{"featuresUrl":"http://geoserver.hel.fi/geoserver/wfs","regionIdTag":"kokotun","nameIdTag":"nimi"}}', 80, 'EPSG:3067', '1.3.0');
+
+
+INSERT INTO oskari_maplayer(type, url,
+                    name, groupId,
+                    locale,
+                    attributes, opacity, srs_name, version)
+VALUES(
+    'statslayer', 'http://geoserver.hel.fi/geoserver/wms',
+    'seutukartta:Seutu_pienalueet', (select id from oskari_layergroup where locale LIKE '%Yhteistyöaineistot%'),
+    '{ "en" : {
+         "name":"Pääkaupunkiseudun pienaluejako"
+       },"fi" : {
+         "name":"Pääkaupunkiseudun pienaluejako"
+    }}',
+    '{"statistics":{"featuresUrl":"http://geoserver.hel.fi/geoserver/wfs","regionIdTag":"kokotun","nameIdTag":"nimi"}}', 80, 'EPSG:3067', '1.3.0');
+
+-- Link layers to datasource
+INSERT INTO
+    oskari_statistical_layer(datasource_id, layer_id)
+VALUES(
+    (SELECT id FROM oskari_statistical_datasource
+        WHERE locale like '%Helsingin kaupunki%'),
+    (SELECT id FROM oskari_maplayer WHERE type='statslayer' AND name = 'seutukartta:Seutu_suuralueet'));
+
+INSERT INTO
+    oskari_statistical_layer(datasource_id, layer_id)
+VALUES(
+    (SELECT id FROM oskari_statistical_datasource
+        WHERE locale like '%Helsingin kaupunki%'),
+    (SELECT id FROM oskari_maplayer WHERE type='statslayer' AND name = 'seutukartta:Seutu_pienalueet'));
---
+++ server-extension/src/main/java/flyway/ptistats/V1_8__link_roles_to_hki_layers.java
@@ -0,0 +1,74 @@
+package flyway.ptistats;
+
+import fi.mml.portti.domain.permissions.Permissions;
+import fi.mml.portti.service.db.permissions.PermissionsService;
+import fi.mml.portti.service.db.permissions.PermissionsServiceIbatisImpl;
+import fi.nls.oskari.domain.Role;
+import fi.nls.oskari.domain.User;
+import fi.nls.oskari.domain.map.OskariLayer;
+import fi.nls.oskari.log.LogFactory;
+import fi.nls.oskari.log.Logger;
+import fi.nls.oskari.map.data.domain.OskariLayerResource;
+import fi.nls.oskari.permission.domain.Permission;
+import fi.nls.oskari.permission.domain.Resource;
+import fi.nls.oskari.service.ServiceException;
+import fi.nls.oskari.service.UserService;
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Inserts viewlayer permission for the kunta & seutukunta layers used as regionsets for the new thematic maps
+ */
+public class V1_8__link_roles_to_hki_layers implements JdbcMigration {
+    private static final Logger LOG = LogFactory.getLogger(V1_8__link_roles_to_hki_layers.class);
+
+    public void migrate(Connection connection)
+            throws SQLException {
+        PermissionsService service = new PermissionsServiceIbatisImpl();
+        for(Resource resToUpdate : getResources()) {
+            Resource dbRes = service.findResource(resToUpdate);
+            if(dbRes != null) {
+                resToUpdate = dbRes;
+            }
+            for(Role role : getRoles()) {
+                if(resToUpdate.hasPermission(role, Permissions.PERMISSION_TYPE_VIEW_LAYER)) {
+                    // already had the permission
+                    continue;
+                }
+                final Permission permission = new Permission();
+                permission.setExternalType(Permissions.EXTERNAL_TYPE_ROLE);
+                permission.setType(Permissions.PERMISSION_TYPE_VIEW_LAYER);
+                permission.setExternalId(Long.toString(role.getId()));
+                resToUpdate.addPermission(permission);
+            }
+            service.saveResourcePermissions(resToUpdate);
+        }
+    }
+
+    // statslayers described as layer resources for permissions handling
+    private List<Resource> getResources() {
+        List<Resource> list = new ArrayList<>();
+        list.add(new OskariLayerResource(OskariLayer.TYPE_STATS, "http://geoserver.hel.fi/geoserver/wms", "seutukartta:Seutu_suuralueet"));
+        list.add(new OskariLayerResource(OskariLayer.TYPE_STATS, "http://geoserver.hel.fi/geoserver/wms", "seutukartta:Seutu_pienalueet"));
+        return list;
+    }
+
+    private List<Role> getRoles() {
+        List<Role> list = new ArrayList<>();
+        try {
+            // "logged in" user
+            list.add(Role.getDefaultUserRole());
+            // guest user
+            User guest = UserService.getInstance().getGuestUser();
+            list.addAll(guest.getRoles());
+        } catch (ServiceException ex) {
+            LOG.warn(ex, "Couldn't get roles listing for statistics layers permissions setup");
+        }
+        return list;
+    }
+
+}
