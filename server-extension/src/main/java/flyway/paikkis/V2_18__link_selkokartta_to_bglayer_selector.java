dbf6420b9ed6e3e4c55da6ea80e5b59fcd63e9fb
Jan Wolski <jan@wolski.fi>
2017-11-13 10:52:41 +0200

    Use layer id instead of external_id

--- server-extension/src/main/java/flyway/paikkis/V2_18__link_selkokartta_to_bglayer_selector.java
+++ server-extension/src/main/java/flyway/paikkis/V2_18__link_selkokartta_to_bglayer_selector.java
@@ -18,7 +18,6 @@ import java.sql.SQLException;
 public class V2_18__link_selkokartta_to_bglayer_selector implements JdbcMigration {
     private final Logger LOG = LogFactory.getLogger(this.getClass());
 
-    private static final String EXTERNAL_ID = "base_selko";
     private static final String PLUGIN_ID = "Oskari.mapframework.bundle.mapmodule.plugin.BackgroundLayerSelectionPlugin";
 
     public void migrate(Connection connection) throws Exception {
@@ -27,9 +26,8 @@ public class V2_18__link_selkokartta_to_bglayer_selector implements JdbcMigratio
             LOG.warn("No Selkokartta layer found, skipping rest of migration!");
             return;
         }
-        updateLayerExternalId(connection, selkokarttaId, EXTERNAL_ID);
 
-        updateViewsWithBaselayerSelector(connection, EXTERNAL_ID);
+        updateViewsWithBaselayerSelector(connection, selkokarttaId);
     }
 
     private Integer getSelkoLayer(Connection conn) throws SQLException {
@@ -45,28 +43,19 @@ public class V2_18__link_selkokartta_to_bglayer_selector implements JdbcMigratio
         return null;
     }
 
-    private void updateLayerExternalId(Connection conn, int layerId, String externalId) throws SQLException {
-        final String sql = "UPDATE oskari_maplayer SET externalid=? WHERE id=?";
-        try (PreparedStatement statement = conn.prepareStatement(sql)) {
-            statement.setString(1, externalId);
-            statement.setInt(2, layerId);
-            statement.execute();
-        }
-    }
-
-    private void updateViewsWithBaselayerSelector(Connection conn, String externalId) throws SQLException {
-        final String sql = "SELECT view_id, seqno, config FROM portti_view_bundle_seq WHERE bundleinstance = 'mapfull'";
+    private void updateViewsWithBaselayerSelector(Connection conn, int selkokarttaId) throws SQLException {
+        final String sql = "SELECT view_id, seqno, config FROM portti_view_bundle_seq WHERE bundle_id = (select id from portti_bundle WHERE name = 'mapfull')";
         try (PreparedStatement statement = conn.prepareStatement(sql)) {
             try (ResultSet rs = statement.executeQuery()) {
                 while (rs.next()) {
                     JSONObject config = JSONHelper.createJSONObject(rs.getString("config"));
-                    updateOneViewIfNeeded(conn, rs.getInt("view_id"), rs.getInt("seqno"), config, externalId);
+                    updateOneViewIfNeeded(conn, rs.getInt("view_id"), rs.getInt("seqno"), config, selkokarttaId);
                 }
             }
         }
     }
 
-    private void updateOneViewIfNeeded(Connection conn, int viewId, int seqNo, JSONObject config, String externalId) throws SQLException {
+    private void updateOneViewIfNeeded(Connection conn, int viewId, int seqNo, JSONObject config, int selkokarttaId) throws SQLException {
         JSONArray plugins = JSONHelper.getJSONArray(config, "plugins");
         for (int i = 0; i < plugins.length(); i++) {
             try {
@@ -83,14 +72,20 @@ public class V2_18__link_selkokartta_to_bglayer_selector implements JdbcMigratio
 
                     boolean hasSelkoLayer = false;
                     for (int j = 0; j < baseLayers.length(); j++) {
-                        if (externalId.equals(baseLayers.getString(j))) {
+                        int current;
+                        try {
+                            current = baseLayers.getInt(j);
+                        } catch (Exception e) {
+                            continue;
+                        }
+                        if (selkokarttaId == current) {
                             hasSelkoLayer = true;
                         }
                     }
                     if (hasSelkoLayer) {
                         continue;
                     }
-                    baseLayers.put(externalId);
+                    baseLayers.put(selkokarttaId);
 
                     final String sql = "UPDATE portti_view_bundle_seq SET config=? WHERE view_id=? AND seqno=?";
                     try (PreparedStatement statement = conn.prepareStatement(sql)) {
@@ -101,7 +96,7 @@ public class V2_18__link_selkokartta_to_bglayer_selector implements JdbcMigratio
                     }
                 }
             } catch (JSONException e) {
-                LOG.error("Error in migration: {}!", e.getMessage());
+                LOG.error("Error in migration:", e.getMessage());
             }
         }
     }
