995b5af8eed1ecdfd83fc8eaa4082a0ea2dd43f2
Sami Mäkinen <sami.makinen@nls.fi>
2017-11-21 17:09:26 +0200

    Fix SQL on ptistats

--- app-resources/src/main/resources/flyway/ptistats/V1_9__update_seutukunnat_to_more_generic_layer.sql
+++ app-resources/src/main/resources/flyway/ptistats/V1_9__update_seutukunnat_to_more_generic_layer.sql
@@ -1,4 +1,4 @@
 -- update layer name
 UPDATE oskari_maplayer SET name='tilastointialueet:seutukunta4500k' where type = 'statslayer' and name = 'tilastointialueet:seutukunta1000k';
 -- update resourcemapping to match the new name
-UPDATE oskari_resource SET name='statslayer+http://geo.stat.fi/geoserver/wms+tilastointialueet:seutukunta4500k' where resource_mapping = 'statslayer+http://geo.stat.fi/geoserver/wms+tilastointialueet:seutukunta1000k';
+UPDATE oskari_resource SET resource_mapping='statslayer+http://geo.stat.fi/geoserver/wms+tilastointialueet:seutukunta4500k' where resource_mapping = 'statslayer+http://geo.stat.fi/geoserver/wms+tilastointialueet:seutukunta1000k';
