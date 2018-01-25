f44d5386f56c9a71efb2dffe3b7e9e953a7dcd52
Sami Mäkinen <sami.makinen@nls.fi>
2018-01-25 13:37:34 +0200

    The rest of OL4 bundles and myplaces migration added

--- app-resources/src/main/resources/flyway/paikkis/V2_24__convert_appsetup_ol4.sql
+++ app-resources/src/main/resources/flyway/paikkis/V2_24__convert_appsetup_ol4.sql
@@ -38,53 +38,45 @@ WITH mod_list AS (
 	SELECT id, t.startup FROM (VALUES
 (
 'mapfull', '{
-   "bundlename": "mapfull",
-   "metadata": {
-     "Import-Bundle": {
-       "maparcgis": {
-         "bundlePath": "/Oskari/packages/mapping/ol3/"
-       },
-       "mapmodule": {
-         "bundlePath": "/Oskari/packages/mapping/ol3/"
-       },
-       "oskariui": {
-         "bundlePath": "/Oskari/packages/framework/bundle/"
-       },
-       "mapwfs2": {
-         "bundlePath": "/Oskari/packages/mapping/ol3/"
-       },
-       "mapstats": {
-         "bundlePath": "/Oskari/packages/mapping/ol3/"
-       },
-       "mapuserlayers": {
-         "bundlePath": "/Oskari/packages/mapping/ol3/"
-       },
-       "ui-components": {
-         "bundlePath": "/Oskari/packages/framework/bundle/"
-       },
-       "mapanalysis": {
-         "bundlePath": "/Oskari/packages/mapping/ol3/"
-       },
-       "mapfull": {
-         "bundlePath": "/Oskari/packages/framework/bundle/"
-       },
-       "mapwmts": {
-         "bundlePath": "/Oskari/packages/mapping/ol3/"
-       }
-     }
-   }
- }'
-),(
-'infobox', '{
-  "bundlename": "infobox",
-  "metadata": {
-    "Import-Bundle": {
-      "infobox": {
-        "bundlePath": "/Oskari/packages/mapping/ol3/"
-      }
-    }
-  }
- }'
+		"metadata": {
+			"Import-Bundle": {
+				"mapmyplaces": {
+					"bundlePath": "/Oskari/packages/mapping/ol3/"
+				},
+				"maparcgis": {
+					"bundlePath": "/Oskari/packages/mapping/ol3/"
+				},
+				"mapmodule": {
+					"bundlePath": "/Oskari/packages/mapping/ol3/"
+				},
+				"oskariui": {
+					"bundlePath": "/Oskari/packages/framework/bundle/"
+				},
+				"mapwfs2": {
+					"bundlePath": "/Oskari/packages/mapping/ol3/"
+				},
+				"mapstats": {
+					"bundlePath": "/Oskari/packages/mapping/ol3/"
+				},
+				"mapuserlayers": {
+					"bundlePath": "/Oskari/packages/mapping/ol3/"
+				},
+				"ui-components": {
+					"bundlePath": "/Oskari/packages/framework/bundle/"
+				},
+				"mapanalysis": {
+					"bundlePath": "/Oskari/packages/mapping/ol3/"
+				},
+				"mapfull": {
+					"bundlePath": "/Oskari/packages/framework/bundle/"
+				},
+				"mapwmts": {
+					"bundlePath": "/Oskari/packages/mapping/ol3/"
+				}
+			}
+		},
+		"bundlename": "mapfull"
+	}'
 ),(
 'toolbar', '{
   "bundlename": "toolbar",
@@ -97,23 +89,56 @@ WITH mod_list AS (
   }
 }'
 ),(
+'infobox', '{
+  "bundlename": "infobox",
+  "metadata": {
+    "Import-Bundle": {
+      "infobox": {
+        "bundlePath": "/Oskari/packages/mapping/ol3/"
+      }
+    }
+  }
+ }'
+),(
+'analyse', '{
+		"metadata": {
+			"Import-Bundle": {
+				"analyse": {
+					"bundlePath": "/Oskari/packages/analysis/ol3/"
+				}
+			}
+		},
+		"bundlename": "analyse"
+	}'
+),(
+'featuredata2', '{
+		"metadata": {
+			"Import-Bundle": {
+				"featuredata2": {
+					"bundlePath": "/Oskari/packages/framework/"
+				}
+			}
+		},
+		"bundlename": "featuredata2"
+	}'
+),(
+'metadatacatalogue', '{
+		"metadata": {
+			"Import-Bundle": {
+				"metadatacatalogue": {
+					"bundlePath": "/Oskari/packages/catalogue/"
+				}
+			}
+		},
+		"bundlename": "metadatacatalogue"
+	}'
+),(
 'heatmap', '{
     "bundlename" : "heatmap",
     "metadata" : {
         "Import-Bundle" : {
             "heatmap" : {
                 "bundlePath" : "/Oskari/packages/mapping/ol3/"
-            }
-        }
-    }
-}'
-),(
-'findbycoordinates', '{
-    "bundlename" : "findbycoordinates",
-    "metadata" : {
-        "Import-Bundle" : {
-            "findbycoordinates": {
-                "bundlePath": "/Oskari/packages/framework/bundle/"
             }
         }
     }
@@ -126,3 +151,16 @@ WHERE
     portti_view.id = portti_view_bundle_seq.view_id AND
     type IN ('DEFAULT', 'USER') AND
     bundle_id IN (SELECT id FROM mod_list);
+
+-- change myplaces2 -> myplaces3
+UPDATE portti_view_bundle_seq SET startup = '{
+    "bundlename" : "myplaces3",
+    "metadata" : {
+       "Import-Bundle" : {
+          "myplaces3" : {
+            "bundlePath" : "/Oskari/packages/framework/bundle/"
+          }
+       }
+    }
+  }', config='{}', state='{}', bundle_id = (select id from portti_bundle where name = 'myplaces3')
+  where bundle_id = (select id from portti_bundle where name = 'myplaces2');
