07abb9125c031e5ea6185e8bc0a671c9cca9530a
Sami Mäkinen <zakarfin@gmail.com>
2018-02-02 17:02:12 +0200

    Fixed json

--- app-resources/src/main/resources/flyway/ptistats/V1_19__update_datasource_names.sql
+++ app-resources/src/main/resources/flyway/ptistats/V1_19__update_datasource_names.sql
@@ -1,5 +1,5 @@
 
 UPDATE oskari_statistical_datasource set locale = '{"fi":{"name":"Helsingin kaupunki"},"sv":{"name":"Helsingfors stad"},"en":{"name":"City of Helsinki"}}' where config like '%http://api.aluesarjat.fi/PXWeb/api/v1/fi/Helsingin%';
 UPDATE oskari_statistical_datasource set locale = '{"fi":{"name":"Sotkanet"},"sv":{"name":"Sotkanet"},"en":{"name":"Sotkanet"}}' where config like '%http://www.sotkanet.fi/rest%';
-UPDATE oskari_statistical_datasource set locale = '{"name":"Kiinteistökauppojen tilastopalvelu"},"sv":{"name":"Köpeskillingsregistret över fastigheter"},"en":{"name":"Official purchase price register"}}' where config like '%https://khr.maanmittauslaitos.fi/tilastopalvelu/rest%';
+UPDATE oskari_statistical_datasource set locale = '{"fi":{"name":"Kiinteistökauppojen tilastopalvelu"},"sv":{"name":"Köpeskillingsregistret över fastigheter"},"en":{"name":"Official purchase price register"}}' where config like '%https://khr.maanmittauslaitos.fi/tilastopalvelu/rest%';
 UPDATE oskari_statistical_datasource set locale = '{"fi":{"name":"Tilastokeskus"},"sv":{"name":"Statistikcentralen"},"en":{"name":"Statistics Finland"}}' where config like '%https://pxnet2.stat.fi/pxweb/api/v1/fi/Kuntien_avainluvut%';
