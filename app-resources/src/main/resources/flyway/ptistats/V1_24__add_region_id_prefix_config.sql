3a376f88be3cbfb13c1ca8f10fc7f77bfaa22226
Sami Mäkinen <sami.makinen@nls.fi>
2018-05-07 16:13:08 +0300

    Add maakunta regionset for TK datasource

--- app-resources/src/main/resources/flyway/ptistats/V1_24__add_region_id_prefix_config.sql
+++ app-resources/src/main/resources/flyway/ptistats/V1_24__add_region_id_prefix_config.sql
@@ -1,5 +1,10 @@
 UPDATE oskari_statistical_layer SET config = '{"regionType":"seutukunta", "valueProcessor": "fi.nls.oskari.control.statistics.plugins.pxweb.parser.PrefixedRegionsValueProcessor", "statsRegionPrefix": "SK"}' 
  where datasource_id = (select id from oskari_statistical_datasource where locale like '%Tilastokeskus%') AND LOWER(config) LIKE '%seutukunta%';
    
-UPDATE oskari_statistical_layer SET config = '{"regionType":"maakunta", "valueProcessor": "fi.nls.oskari.control.statistics.plugins.pxweb.parser.PrefixedRegionsValueProcessor", "statsRegionPrefix": "MK"}' 
- where datasource_id = (select id from oskari_statistical_datasource where locale like '%Tilastokeskus%') AND LOWER(config) LIKE '%maakunta%';
+INSERT INTO
+    oskari_statistical_layer(datasource_id, layer_id, config)
+VALUES(
+    (SELECT id FROM oskari_statistical_datasource
+        WHERE locale like '%Tilastokeskus%'),
+    (SELECT id FROM oskari_maplayer WHERE type='statslayer' AND name = 'tilastointialueet:maakunta4500k'),
+    '{"regionType":"MAAKUNTA, "valueProcessor": "fi.nls.oskari.control.statistics.plugins.pxweb.parser.PrefixedRegionsValueProcessor", "statsRegionPrefix": "MK""}');
