bc20a80be93ba9a77dc21a28ed6c764a41481a03
Sami Mäkinen <sami.makinen@nls.fi>
2018-05-07 16:05:15 +0300

    Add region-prefix config for TK layers

---
+++ app-resources/src/main/resources/flyway/ptistats/V1_22__add_region_id_prefix_config.sql
@@ -0,0 +1,5 @@
+UPDATE oskari_statistical_layer SET config = '{"regionType":"seutukunta", "valueProcessor": "fi.nls.oskari.control.statistics.plugins.pxweb.parser.PrefixedRegionsValueProcessor", "statsRegionPrefix": "SK"}' 
+ where datasource_id = (select id from oskari_statistical_datasource where locale like '%Tilastokeskus%') AND LOWER(config) LIKE '%seutukunta%';
+   
+UPDATE oskari_statistical_layer SET config = '{"regionType":"maakunta", "valueProcessor": "fi.nls.oskari.control.statistics.plugins.pxweb.parser.PrefixedRegionsValueProcessor", "statsRegionPrefix": "MK"}' 
+ where datasource_id = (select id from oskari_statistical_datasource where locale like '%Tilastokeskus%') AND LOWER(config) LIKE '%maakunta%';
