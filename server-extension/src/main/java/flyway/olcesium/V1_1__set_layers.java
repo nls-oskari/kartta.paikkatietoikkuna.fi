ca071a135a732da9beae3dd435f5515d0fa62300
Joonas Heijari <joonas.heijari@gmail.com>
2018-05-16 12:20:16 +0300

    Save capabilities to cache. Refactored for better readability

--- server-extension/src/main/java/flyway/olcesium/V1_1__set_layers.java
+++ server-extension/src/main/java/flyway/olcesium/V1_1__set_layers.java
@@ -27,11 +27,11 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class V1_1__set_layers implements JdbcMigration {
 
     private static final Logger LOG = LogFactory.getLogger(flyway.olcesium.V1_1__set_layers.class);
-    private static final String PROJECTION_PSEUDO_MERCATOR = "EPSG:3857";
     private static final String CESIUM_VIEW_NAME = "Geoportal Ol Cesium";
     private static final String MAP_BUNDLE_NAME = "mapfull";
     private static final String BASELAYER_PLUGIN_NAME = "BackgroundLayerSelectionPlugin";
@@ -54,28 +54,28 @@ public class V1_1__set_layers implements JdbcMigration {
 
     private void updateNlsBaseLayer(Connection connection, OskariLayer layer) {
         try {
-            // update
-            OskariLayerCapabilities caps = capabilitiesService.getCapabilities(layer);
-            if (caps != null) {
-                WMTSCapabilities parsed = WMTSCapabilitiesParser.parseCapabilities(caps.getData());
-                if (parsed != null) {
-                    WMTSCapabilitiesLayer capsLayer = parsed.getLayer(layer.getName());
-                    if (capsLayer != null) {
-                        JSONObject jscaps = LayerJSONFormatterWMTS.createCapabilitiesJSON(capsLayer, (Set)null);
-                        if (jscaps != null) {
-                            updateCapabilities(layer.getId(), jscaps, connection);
-                        }else {
-                            LOG.info("WMTSCapabilities / json create failed - layer: ", layer.getName());
-                        }
-                    } else {
-                        LOG.info("WMTSCapabilities / layer parse failed - layer: ", layer.getName());
-                    }
-                } else {
-                    LOG.info("WMTSCapabilities / Capabilities parse failed - layer: ", layer.getName());
-                }
+            // update capabilities in cache
+            String data = CapabilitiesCacheService.getFromService(layer);
+            OskariLayerCapabilities caps = new OskariLayerCapabilities(
+                    layer.getSimplifiedUrl(true),
+                    layer.getType(),
+                    layer.getVersion(),
+                    data);
+            capabilitiesService.save(caps);
+
+            WMTSCapabilities parsed = WMTSCapabilitiesParser.parseCapabilities(caps.getData());
+            WMTSCapabilitiesLayer capsLayer = parsed.getLayer(layer.getName());
+
+            JSONObject jscaps = null;
+            if (capsLayer != null) {
+                jscaps = LayerJSONFormatterWMTS.createCapabilitiesJSON(capsLayer, (Set)null);
             } else {
-                LOG.info("WMTSCapabilities / getCapabilities failed - layer: ", layer.getName());
+                LOG.info("WMTSCapabilities / layer parse failed - layer: ", layer.getName());
+                return;
             }
+
+            updateCapabilities(layer.getId(), jscaps, connection);
+
         } catch (Exception e) {
             LOG.error(e, "Error getting capabilities for layer", layer);
         }
@@ -83,25 +83,24 @@ public class V1_1__set_layers implements JdbcMigration {
 
     private void updateCesiumViews(Connection connection, List<OskariLayer> layers) {
         try {
-            List<View> views = getCesiumViews(connection);
-            LOG.info("Updating Cesium views - count:", views.size());
+            List<Long> viewIds = getCesiumViewIds(connection);
+            LOG.info("Updating Cesium views - count:", viewIds.size());
 
             JSONArray selectedLayers = new JSONArray();
             JSONObject jsonLyr = new JSONObject();
             jsonLyr.put("id", layers.get(0).getId());
             selectedLayers.put(jsonLyr);
 
-            JSONArray layerSelectionPluginLayers = new JSONArray();
-            layers.stream().forEach((layer) -> {
-                try {
-                    layerSelectionPluginLayers.put(layer.getId());
-                } catch (Exception ex) {
-                    LOG.warn("Updating Cesium views / could not list layers to background selection plugin");
-                }
-            });
+            List lyrIds = null;
+            try {
+                lyrIds = layers.stream().map((layer) -> layer.getId()).collect(Collectors.toList());
+            } catch (Exception e) {
+                LOG.error(e, "Error adding layers to selection plugin for cesium views");
+            }
+            JSONArray layerSelectionPluginLayers = new JSONArray(lyrIds);
 
-            for(View view : views) {
-                View modifyView = viewService.getViewWithConf(view.getId());
+            for(Long viewId : viewIds) {
+                View modifyView = viewService.getViewWithConf(viewId);
                 Bundle mapBundle = modifyView.getBundleByName(MAP_BUNDLE_NAME);
                 if (mapBundle != null) {
 
@@ -109,35 +108,42 @@ public class V1_1__set_layers implements JdbcMigration {
                     if (state != null) {
                         state.put("selectedLayers", selectedLayers);
                         mapBundle.setState(state.toString());
-                        viewService.updateBundleSettingsForView(view.getId(), mapBundle);
+                        viewService.updateBundleSettingsForView(viewId, mapBundle);
                     } else {
-                        LOG.warn("Updating Cesium views / mapfull state is null - view: ", view.getId());
+                        LOG.warn("Updating Cesium views / mapfull state is null - view: ", viewId);
                     }
 
-                    JSONObject config = JSONHelper.createJSONObject(mapBundle.getConfig());
-                    if (config != null) {
-                        JSONArray plugins = config.optJSONArray("plugins");
-                        if (plugins != null) {
-                            for (int i = 0; i < plugins.length(); i++) {
-                                JSONObject plugin = plugins.getJSONObject(i);
-                                if (plugin.getString("id").contains(BASELAYER_PLUGIN_NAME)) {
-                                    JSONObject pluginConf = plugin.optJSONObject("config");
-                                    if (pluginConf == null) {
-                                        pluginConf = new JSONObject();
-                                        plugin.put("config", pluginConf);
+                    if (layerSelectionPluginLayers != null) {
+                        JSONObject config = JSONHelper.createJSONObject(mapBundle.getConfig());
+                        if (config != null) {
+                            JSONArray plugins = config.optJSONArray("plugins");
+                            JSONObject layerSelectionPlugin = null;
+                            if (plugins != null) {
+                                for (int i = 0; i < plugins.length(); i++) {
+                                    JSONObject plugin = plugins.getJSONObject(i);
+                                    if (plugin.getString("id").contains(BASELAYER_PLUGIN_NAME)) {
+                                        layerSelectionPlugin = plugin;
+                                        break;
                                     }
-                                    pluginConf.put("baseLayers", layerSelectionPluginLayers);
                                 }
                             }
-                        }
-                        mapBundle.setConfig(config.toString());
-                        viewService.updateBundleSettingsForView(view.getId(), mapBundle);
-                    } else {
-                        LOG.warn("Updating Cesium views / mapfull condig is null - view: ", view.getId());
-                    }
+                            if (layerSelectionPlugin != null) {
+                                JSONObject pluginConf = layerSelectionPlugin.optJSONObject("config");
+                                if (pluginConf == null) {
+                                    pluginConf = new JSONObject();
+                                    layerSelectionPlugin.put("config", pluginConf);
+                                }
+                                pluginConf.put("baseLayers", layerSelectionPluginLayers);
 
+                                mapBundle.setConfig(config.toString());
+                                viewService.updateBundleSettingsForView(viewId, mapBundle);
+                            }
+                        } else {
+                            LOG.warn("Updating Cesium views / mapfull condig is null - view: ", viewId);
+                        }
+                    }
                 } else {
-                    LOG.warn("Updating Cesium views / mapfull bundle not found - view: ", view.getId());
+                    LOG.warn("Updating Cesium views / mapfull bundle not found - view: ", viewId);
                 }
             }
         } catch (Exception e) {
@@ -145,16 +151,15 @@ public class V1_1__set_layers implements JdbcMigration {
         }
     }
 
-    private List<View> getCesiumViews(Connection conn) throws SQLException {
-        List<View> list = new ArrayList<>();
-        final String sql = "SELECT id FROM portti_view WHERE name = '"+CESIUM_VIEW_NAME+"'";
+    private List<Long> getCesiumViewIds(Connection conn) throws SQLException {
+        List<Long> list = new ArrayList<>();
+        final String sql = "SELECT id FROM portti_view WHERE name=?";
 
         try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setString(1, CESIUM_VIEW_NAME);
             try (ResultSet rs = statement.executeQuery()) {
                 while (rs.next()) {
-                    View view = new View();
-                    view.setId(rs.getLong("id"));
-                    list.add(view);
+                    list.add(rs.getLong("id"));
                 }
             }
         } catch (Exception e) {
@@ -172,7 +177,7 @@ public class V1_1__set_layers implements JdbcMigration {
             statement.execute();
         }
         catch (JSONException ignored) {
-            LOG.error("Error updating oskari_maplayer.capabilities and attributes", layerId);
+            LOG.error("Error updating oskari_maplayer.capabilities", layerId);
         }
     }
 
