3d697fef46a8c9530affa474dd441955c9b29134
Sami Mäkinen <sami.makinen@nls.fi>
2019-06-19 14:09:10 +0300

    Remove ptimigration (liferay -> jetty) and olcesium (replaced by pti3d) flyway modules

--- app-resources/src/main/resources/flyway/olcesium/V1_17__cleanup.sql
+++
@@ -1,6 +0,0 @@
--- Remove 3D views
-delete from portti_view where page = 'index3D' and application = 'full-map_experimental';
-
--- Remove 3D link from the default and user views
-delete from portti_view_bundle_seq where bundle_id = (select id from portti_bundle where name = 'demo-link');
-delete from portti_bundle where name = 'demo-link';
--- app-resources/src/main/resources/flyway/ptimigration/V1_9__alter_view_names.sql
+++
@@ -1,2 +0,0 @@
--- Update default view names so page title is cleaner
-update portti_view set name = 'Paikkatietoikkuna' where id in (4438, 1);
