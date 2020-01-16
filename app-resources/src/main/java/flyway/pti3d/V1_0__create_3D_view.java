7f9e3e816ecd9698928f9d915cfc3174d673cd26
Sami Mäkinen <zakarfin@gmail.com>
2020-01-16 18:06:44 +0200

    Register bundle on migration to prevent duplicate registration

--- app-resources/src/main/java/flyway/pti3d/V1_0__create_3D_view.java
+++ app-resources/src/main/java/flyway/pti3d/V1_0__create_3D_view.java
@@ -1,6 +1,8 @@
 package flyway.pti3d;
 
+import fi.nls.oskari.db.BundleHelper;
 import fi.nls.oskari.db.DBHandler;
+import fi.nls.oskari.domain.map.view.Bundle;
 import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
 
 import java.sql.Connection;
@@ -11,6 +13,11 @@ import java.sql.Connection;
 public class V1_0__create_3D_view implements JdbcMigration {
 
     public void migrate(Connection connection) throws Exception {
+        // ensure we have the bundle
+        Bundle b  = new Bundle();
+        b.setName("dimension-change");
+        BundleHelper.registerBundle(b, connection);
+
         DBHandler.setupAppContent(connection, "paikkis-3D.json");
     }
 }
