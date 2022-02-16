0dd19fc47cb2b17115fc06352641bfb47a992ced
Sami Mäkinen <sami.makinen@nls.fi>
2022-02-16 16:20:43 +0200

    Metadata has new fields and valueType is no longer used

--- scripts/pxweb/transform_metadata.js
+++ scripts/pxweb/transform_metadata.js
@@ -26,6 +26,42 @@ const getLocalized = (value) => {
         fi: value
     };
 };
+const getNum = (value, row) => {
+    if (value == 'Koko maa, Kunta, Maakunta, Seutukunta') {
+        console.log('Unexpected value for num. Check parsing for row:', row);
+        return undefined;
+    }
+    if ('ääretön' === value) {
+        //console.log('Number.POSITIVE_INFINITY')
+        return 'Number.POSITIVE_INFINITY'
+    } else if ('negat ääretön' === value) {
+        //console.log('Number.NEGATIVE_INFINITY')
+        return 'Number.NEGATIVE_INFINITY'
+    }
+    let num = value;
+    try {
+        num = Number.parseInt(value);
+        if (isNaN(num)) {
+            num = undefined;
+        }
+    } catch(e) {
+        num =  undefined;
+    }
+    if (typeof num === 'undefined') {
+        console.log("num: '" + value + "' -> '" + num + "'");
+    }
+    return num;
+}
+
+const isRatio = (lkm) => {
+    if (lkm === 'lkm') {
+        return false;
+    } else if (lkm === 'suhde') {
+        return true;
+    }
+    console.log("Unrecognized ratio value: '" + lkm + "'");
+    return undefined;
+}
 
 const rows = contents.split('\n');
 const jsonItems = rows.map(row => {
@@ -33,9 +69,23 @@ const jsonItems = rows.map(row => {
     CSV-columns by Tilastokeskus:
     Koodi;Indikaattorin nimi;Tuottava tilasto;Tilastolyhenne;SVT aihealue;Järjestys;
     LAST-UPDATE;NEXT-UPDATE;FIRST DATA;LAST DATA;
-    Kuvaus;Laskentakaava;Esitettävien desimaalien määrä;Mihin palveluihin kuuluu;Lukutyyppi;Aluejaot
+    Kuvaus;Laskentakaava;Esitettävien desimaalien määrä;Mihin palveluihin kuuluu;Lukutyyppi;Aluejaot;min;max;base;lkm
     */
-    const cols = row.split(';');
+    let cols = row.split(';');
+    let skip;
+    cols = cols.map((c, index) => {
+        if (index == skip) {
+            return null;
+        }
+        if (c.startsWith("\"")) {
+            // NOTE! assumes there's only one ; inside quoted value.
+            // We should just generate the CSV using tab-separator to workaround ; in cell values...
+            console.log('Value had inner ;', cols.join(';'));
+            skip = index + 1;
+            return c + cols[skip];
+        }
+        return c;
+    }).filter(i => i !== null);
     if (cols.length < 10) {
         return;
     }
@@ -45,6 +95,7 @@ const jsonItems = rows.map(row => {
 //        name: getLocalized(cols[1]),
         desc: getLocalized(cols[10]),
         source: getLocalized(cols[2]),
+        // there's no valueType anymore. Frontend should use min, max, base, isRatio to detect it.
         valueType: VALUE_TYPE[cols[14]],
         decimalCount: Number(cols[12]),
         timerange: {
@@ -57,6 +108,11 @@ const jsonItems = rows.map(row => {
         // - commented out since not used/labels: cols[4] ? cols[4].split(',').map(l => l.trim()) : [],
         // - commented out since not used/prio: Number(cols[5]),
         // - commented out since not used/regionsets: cols[15] ? cols[15].split(',').map(l => l.trim()): []
+        // ;min;max;base;lkm
+        min: getNum(cols[16], row),
+        max: getNum(cols[17], row),
+        base: getNum(cols[18], row),
+        isRatio: isRatio(cols[19].trim())
     }
 })
 .filter(item => !!item)
