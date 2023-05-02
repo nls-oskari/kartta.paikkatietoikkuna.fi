e4e069e1666199fdacdf5c34d826e14fdbc5e283
Sami Mäkinen <sami.makinen@nls.fi>
2023-05-02 15:01:45 +0300

    Remove link to SotkaNet

--- app-resources/src/main/resources/flyway/ptistats/V1_49__add_hyvinvointi.sql
+++ app-resources/src/main/resources/flyway/ptistats/V1_49__add_hyvinvointi.sql
@@ -23,14 +23,6 @@ INSERT INTO oskari_maplayer_group_link (maplayerid, groupid, order_number)
      FROM oskari_maplayer WHERE type='statslayer' AND name = 'tilastointialueet:hyvinvointialue4500k';
 
 INSERT INTO
-    oskari_statistical_datasource_regionsets(datasource_id, layer_id, config)
-VALUES(
-    (SELECT id FROM oskari_statistical_datasource
-        WHERE locale like '%Sotkanet%'),
-    (SELECT id FROM oskari_maplayer WHERE type='statslayer' AND name = 'tilastointialueet:hyvinvointialue4500k'),
-    '{"regionType":"HYVINVOINTIALUE"}');
-
-INSERT INTO
     oskari_statistical_datasource_regionsets(datasource_id, layer_id)
 VALUES(
     (SELECT id FROM oskari_statistical_datasource
