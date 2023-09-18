d1d78d63d11f44ff97dcec3fa17ecff47f44e002
Pekka Helesuo <pekka.helesuo@siili.com>
2023-09-18 14:54:23 +0300

    add null checks

--- app-resources/src/main/java/flyway/pti/V3_3_2__migrate_created_and_last_login.java
+++ app-resources/src/main/java/flyway/pti/V3_3_2__migrate_created_and_last_login.java
@@ -18,6 +18,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class V3_3_2__migrate_created_and_last_login extends BaseJavaMigration {
     class User {
         Long id;
@@ -29,6 +30,8 @@ public class V3_3_2__migrate_created_and_last_login extends BaseJavaMigration {
 
     private Logger log = LogFactory.getLogger(V3_3_2__migrate_created_and_last_login.class);
 
+    private final String CREATED_JSON_KEY = "created";
+    private final String LAST_LOGIN_JSON_KEY = "lastLogin";
     @Override
     public void migrate(Context context) throws Exception {
         Connection connection = context.getConnection();
@@ -36,13 +39,28 @@ public class V3_3_2__migrate_created_and_last_login extends BaseJavaMigration {
         users.forEach(user -> {
             JSONObject attributes = JSONHelper.createJSONObject(user.attributes);
             try {
-                String created = attributes.getString("created");
-                String lastLogin = attributes.getString("lastLogin");
-                attributes.remove("created");
-                attributes.remove("lastLogin");
+
+                String created = null;
+                String lastLogin = null;
+                if (attributes.has(CREATED_JSON_KEY)) {
+                    created = attributes.getString(CREATED_JSON_KEY);
+                    attributes.remove(CREATED_JSON_KEY);
+                }
+                if (attributes.has(LAST_LOGIN_JSON_KEY)) {
+                    lastLogin = attributes.getString(LAST_LOGIN_JSON_KEY);
+                    attributes.remove(LAST_LOGIN_JSON_KEY);
+                }
+
                 user.attributes = attributes.toString();
-                user.created = this.formatDateTime(created);
-                user.lastLogin = this.formatDateTime(lastLogin);
+
+                if (created != null) {
+                    user.created = this.formatDateTime(created);
+                }
+
+                if (lastLogin != null) {
+                    user.lastLogin = this.formatDateTime(lastLogin);
+                }
+
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
@@ -53,13 +71,21 @@ public class V3_3_2__migrate_created_and_last_login extends BaseJavaMigration {
 
     public List<User> getUsers(Connection conn) throws Exception {
         List<User> users = new ArrayList<>();
-        final String sql = "SELECT id, user_name, attributes FROM oskari_users;";
+        final String sql = "SELECT id, user_name, attributes, created, last_login FROM oskari_users";
         try(PreparedStatement statement = conn.prepareStatement(sql)) {
             try (ResultSet rs = statement.executeQuery()) {
                 while(rs.next()) {
                     User user = new User();
                     user.id = rs.getLong("id");
                     user.attributes = rs.getString("attributes");
+                    OffsetDateTime created = rs.getObject("created", OffsetDateTime.class);
+                    OffsetDateTime lastLogin = rs.getObject("last_login", OffsetDateTime.class);
+                    if (created != null) {
+                        user.created = created;
+                    }
+                    if (lastLogin != null) {
+                        user.lastLogin = lastLogin;
+                    }
                     users.add(user);
                 }
             }
@@ -80,7 +106,6 @@ public class V3_3_2__migrate_created_and_last_login extends BaseJavaMigration {
     }
 
     private void saveChanges(Connection conn, List<User> users) throws SQLException {
-//        final String sql = "UPDATE oskari_users_migration_test SET attributes = ?, created = ? , last_login = ? WHERE id = ?";
         final String sql = "UPDATE oskari_users SET attributes = ?, created = ? , last_login = ? WHERE id = ?";
         try (PreparedStatement ps = conn.prepareStatement(sql)) {
             for (User user : users) {
