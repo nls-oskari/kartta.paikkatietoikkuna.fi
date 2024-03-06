482f7f2afa892fcb314988545b5afdb7942ed1f9
Sami Mäkinen <sami.makinen@nls.fi>
2024-03-06 13:22:38 +0200

    Remove featuresUrl, add rest of HKI layer updates

--- app-resources/src/main/resources/flyway/ptistats/V1_61__fix_hki_layers.sql
+++ app-resources/src/main/resources/flyway/ptistats/V1_61__fix_hki_layers.sql
@@ -3,20 +3,42 @@ UPDATE oskari_maplayer
 SET
     url = 'https://kartta.hel.fi/ws/geoserver/avoindata/wfs',
     name = 'avoindata:Seutukartta_aluejako_pienalue',
-    attributes = '{"statistics":{"featuresUrl": "https://kartta.hel.fi/ws/geoserver/avoindata/wfs","regionIdTag":"kokotun","nameIdTag":"nimi"}}'
+    attributes = '{"statistics":{"regionIdTag":"kokotun","nameIdTag":"nimi"}}'
 WHERE type='statslayer' AND name = 'seutukartta:Seutu_pienalueet';
 
 UPDATE oskari_maplayer
 SET
     url = 'https://kartta.hel.fi/ws/geoserver/avoindata/wfs',
     name = 'avoindata:Seutukartta_aluejako_suuralue',
-    attributes = '{"statistics":{"featuresUrl": "https://kartta.hel.fi/ws/geoserver/avoindata/wfs","regionIdTag":"kokotun","nameIdTag":"nimi"}}'
+    attributes = '{"statistics":{"regionIdTag":"kokotun","nameIdTag":"nimi"}}'
 WHERE type='statslayer' AND name = 'seutukartta:Seutu_suuralueet';
 
 UPDATE oskari_maplayer
 SET
     url = 'https://kartta.hel.fi/ws/geoserver/avoindata/wfs',
     name = 'avoindata:Seutukartta_aluejako_tilastoalue',
-    attributes = '{"statistics":{"featuresUrl": "https://kartta.hel.fi/ws/geoserver/avoindata/wfs","regionIdTag":"kokotun","nameIdTag":"nimi"}}'
+    attributes = '{"statistics":{"regionIdTag":"kokotun","nameIdTag":"nimi"}}'
 WHERE type='statslayer' AND name = 'seutukartta:Seutu_tilastoalueet';
+
+UPDATE oskari_maplayer
+SET
+    url = 'https://kartta.hel.fi/ws/geoserver/avoindata/wfs',
+    name = 'avoindata:Piirijako_suurpiiri',
+    attributes = '{"statistics":{"regionIdTag":"kokotun","nameIdTag":"nimi"}}'
+WHERE type='statslayer' AND name = 'hel:Helsinki_suurpiirit';
+
+UPDATE oskari_maplayer
+SET
+    url = 'https://kartta.hel.fi/ws/geoserver/avoindata/wfs',
+    name = 'avoindata:Piirijako_peruspiiri',
+    attributes = '{"statistics":{"regionIdTag":"kokotun","nameIdTag":"nimi"}}'
+WHERE type='statslayer' AND name = 'hel:Helsinki_peruspiirit';
+
+UPDATE oskari_maplayer
+SET
+    url = 'https://kartta.hel.fi/ws/geoserver/avoindata/wfs',
+    name = 'avoindata:Piirijako_osaalue',
+    attributes = '{"statistics":{"regionIdTag":"kokotun","nameIdTag":"nimi"}}'
+WHERE type='statslayer' AND name = 'seutukartta:Helsinki_osa-alueet';
+
 
